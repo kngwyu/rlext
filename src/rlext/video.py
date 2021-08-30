7792c72fe24dc7cad4275eee6c40df6ccdfe9608
kngwyu <yuji.kngw.80s.revive@gmail.com>
2021-08-30 17:33:40 +0900

    SubProcessVideoWriter

--- src/rlext/video.py
+++ src/rlext/video.py
@@ -15,6 +15,7 @@ class VideoWriter:
         image_shape: str = "HWC",
         fourcc: str = "XVID",
         rgb: bool = True,
+        _close_at_exit: bool = True,
     ) -> None:
         self._path = path
         self._writer = None
@@ -38,7 +39,8 @@ class VideoWriter:
                 return transposed
 
         self._convert = convert
-        atexit.register(self.close)
+        if _close_at_exit:
+            atexit.register(self.close)
 
     def _initialize_writer(self, image: np.ndarray) -> None:
         shape = image.shape
@@ -61,3 +63,56 @@ class VideoWriter:
     def close(self) -> None:
         if self._writer is not None:
             self._writer.release()
+
+
+class SubProcessVideoWriter:
+    def __init__(
+        self,
+        path: Path,
+        fps: float = 20.0,
+        image_shape: str = "HWC",
+        fourcc: str = "XVID",
+        rgb: bool = True,
+    ) -> None:
+        import multiprocessing as mp
+        import traceback
+
+        class Worker(mp.Process):
+            def __init__(self, pipe: "Connection") -> None:
+                super().__init__()
+                self._writer = VideoWriter(
+                    path=path,
+                    fps=fps,
+                    image_shape=image_shape,
+                    fourcc=fourcc,
+                    rgb=rgb,
+                    _close_at_exit=False,
+                )
+                self._pipe = pipe
+
+            def run(self) -> None:
+                try:
+                    while True:
+                        image = self._pipe.recv()
+                        if image is None:
+                            self._writer.close()
+                            self._pipe.close()
+                            break
+                        else:
+                            self._writer.append(image)
+                except Exception as e:
+                    traceback.print_exc()
+                    print("Exception occured in videowriter process")
+                    raise e
+
+        self._pipe, worker_pipe = mp.Pipe()
+        self._worker = Worker(worker_pipe)
+        self._worker.start()
+        atexit.register(self.close)
+
+    def append(self, image: np.ndarray) -> None:
+        self._pipe.send(image)
+
+    def close(self) -> None:
+        self._pipe.send(None)
+        self._pipe.close()
